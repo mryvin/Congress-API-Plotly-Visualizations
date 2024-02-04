70a5261b510958cd6955ea2a479f7b1418b5f40a
mryvin <89282726+mryvin@users.noreply.github.com>
2024-02-04 17:48:58 -0500

    Update bill_data_collection.py

--- scripts/data/bill_data_collection.py
+++ scripts/data/bill_data_collection.py
@@ -1,22 +1,39 @@
 import json
 import os
-
-from dotenv import load_dotenv
 import requests
+from dotenv import load_dotenv
 
-### CONSTANTS
+# Load environment variables
 load_dotenv()
+
+# Set API key and limit constants
 API_KEY = os.getenv("API_KEY")
 LIMIT = 50 # default 20, max 250
 
+###############################################################################################################################################################
+##### Bills
+###############################################################################################################################################################
+
 def get_bills():
+    """
+    Fetches bills from the API and writes their details to a JSON file.
+    """
     offset = 0
     bills = []
+
+    # Fetch bills in batches of size LIMIT
     while True:
-        print(f"Getting bills [{offset}, {offset + LIMIT})")
+        print(f"Fetching bills [{offset}, {offset + LIMIT})")
+
+        # Make API request
         r = requests.get("https://api.congress.gov/v3/bill", params={"api_key": API_KEY, "offset": offset, "limit": LIMIT})
+
+        # Check for successful request
         if r.status_code != 200:
             print("Error with API request")
+            break
+
+        # Extract bill details
         r_bills = r.json()["bills"]
         if not r_bills:
             break
@@ -26,31 +43,38 @@ def get_bills():
                 "type": r_bill.get("type"),
                 "number": r_bill.get("number")
             })
-        offset += 250
-        if offset > 50: break # TODO: remove this later
 
+        # Update offset for next batch
+        offset += LIMIT
+
+    # Write bill details to file
     with open("bills.json", "w") as f:
         f.write(json.dumps(bills))
 
-    bills_with_details = []
-    for bill in bills:
-        bills_with_details.append(get_bill_details(bill["congress"], bill["type"], bill["number"]))
-    
-    for bill in bills_with_details:
-        bill["actions"] = get_bill_actions(bill["congress"], bill["bill_type"], bill["bill_number"])
-        bill["committees"] = get_bill_committees(bill["congress"], bill["bill_type"], bill["bill_number"])
-        bill["cosponsors"] = get_bill_cosponsors(bill["congress"], bill["bill_type"], bill["bill_number"])
-        bill["relatedbills"] = get_bill_relatedbills(bill["congress"], bill["bill_type"], bill["bill_number"])
-        bill["subjects"] = get_bill_subjects(bill["congress"], bill["bill_type"], bill["bill_number"])
-
+    # Fetch and write detailed bill information
+    bills_with_details = [get_bill_details(bill["congress"], bill["type"], bill["number"]) for bill in bills]
     with open("bills_with_details.json", "w") as f:
         f.write(json.dumps(bills_with_details))
 
+###############################################################################################################################################################
+##### Bill Details
+###############################################################################################################################################################
+
 def get_bill_details(congress, bill_type, bill_number):
-    print(f"Getting details for {congress}/{bill_type}/{bill_number}")
+    """
+    Fetches and returns details for a specific bill.
+    """
+    print(f"Fetching details for {congress}/{bill_type}/{bill_number}")
+
+    # Make API request
     r = requests.get(f"https://api.congress.gov/v3/bill/{congress}/{bill_type}/{bill_number}", params={"api_key": API_KEY})
+
+    # Check for successful request
     if r.status_code != 200:
         print("Error with API request")
+        return {}
+
+    # Extract bill details
     r_bill = r.json()["bill"]
     return {
         "actions_count": r_bill.get("actions", {}).get("count"),
@@ -73,61 +97,115 @@ def get_bill_details(congress, bill_type, bill_number):
         "bill_number": bill_number
     }
 
+###############################################################################################################################################################
+##### Bill Actions
+###############################################################################################################################################################
+
 def get_bill_actions(congress, bill_type, bill_number):
-    print(f"Getting actions for {congress}/{bill_type}/{bill_number}")
+    """
+    Fetches and returns actions for a specific bill.
+    """
+    print(f"Fetching actions for {congress}/{bill_type}/{bill_number}")
+
+    # Make API request
     r = requests.get(f"https://api.congress.gov/v3/bill/{congress}/{bill_type}/{bill_number}/actions", params={"api_key": API_KEY})
+
+    # Check for successful request
     if r.status_code != 200:
         print("Error with API request")
+        return []
+
+    # Extract and return action details
     r_actions = r.json()["actions"]
-    actions = []
-    for action in r_actions:
-        # NOTE: conflicts with actions_count if we remove dupes, review this
-        action_date_type = {"date": action["actionDate"], "type": action["type"]}
-        if action_date_type not in actions:
-            actions.append(action_date_type)
-    return actions
+    return [{"date": action["actionDate"], "type": action["type"]} for action in r_actions]
+
+###############################################################################################################################################################
+##### Bill Committees
+###############################################################################################################################################################
 
 def get_bill_committees(congress, bill_type, bill_number):
-    print(f"Getting committees for {congress}/{bill_type}/{bill_number}")
+    """
+    Fetches and returns committees for a specific bill.
+    """
+    print(f"Fetching committees for {congress}/{bill_type}/{bill_number}")
+
+    # Make API request
     r = requests.get(f"https://api.congress.gov/v3/bill/{congress}/{bill_type}/{bill_number}/committees", params={"api_key": API_KEY})
+
+    # Check for successful request
     if r.status_code != 200:
         print("Error with API request")
+        return []
+
+    # Extract and return committee details
     r_committees = r.json()["committees"]
-    committees = []
-    for committee in r_committees:
-        committees.append({"name": committee["name"], "system_code": committee["systemCode"]})
-    return committees
+    return [{"name": committee["name"], "system_code": committee["systemCode"]} for committee in r_committees]
+
+###############################################################################################################################################################
+##### Bill Cosponsors
+###############################################################################################################################################################
 
 def get_bill_cosponsors(congress, bill_type, bill_number):
-    print(f"Getting cosponsors for {congress}/{bill_type}/{bill_number}")
+    """
+    Fetches and returns cosponsors for a specific bill.
+    """
+    print(f"Fetching cosponsors for {congress}/{bill_type}/{bill_number}")
+
+    # Make API request
     r = requests.get(f"https://api.congress.gov/v3/bill/{congress}/{bill_type}/{bill_number}/cosponsors", params={"api_key": API_KEY})
+
+    # Check for successful request
     if r.status_code != 200:
         print("Error with API request")
+        return []
+
+    # Extract and return cosponsor details
     r_cosponsors = r.json()["cosponsors"]
-    cosponsors = []
-    for cosponsor in r_cosponsors:
-        cosponsors.append({"bioguide_id": cosponsor["bioguideId"], "party": cosponsor["party"], "state": cosponsor["state"], "district": cosponsor.get("district")})
-    return cosponsors
+    return [{"bioguide_id": cosponsor["bioguideId"], "party": cosponsor["party"], "state": cosponsor["state"], "district": cosponsor.get("district")} for cosponsor in r_cosponsors]
+
+###############################################################################################################################################################
+##### Related Bills
+###############################################################################################################################################################
 
 def get_bill_relatedbills(congress, bill_type, bill_number):
-    print(f"Getting related bills for {congress}/{bill_type}/{bill_number}")
+    """
+    Fetches and returns related bills for a specific bill.
+    """
+    print(f"Fetching related bills for {congress}/{bill_type}/{bill_number}")
+
+    # Make API request
     r = requests.get(f"https://api.congress.gov/v3/bill/{congress}/{bill_type}/{bill_number}/relatedbills", params={"api_key": API_KEY})
+
+    # Check for successful request
     if r.status_code != 200:
         print("Error with API request")
+        return []
+
+    # Extract and return related bill details
     r_relatedbills = r.json()["relatedBills"]
-    relatedbills = []
-    for relatedbill in r_relatedbills:
-        relatedbills.append({"related_bill_congress": relatedbill["congress"], "related_bill_number": relatedbill["number"], "related_bill_type": relatedbill["type"]})
-    return relatedbills
+    return [{"related_bill_congress": relatedbill["congress"], "related_bill_number": relatedbill["number"], "related_bill_type": relatedbill["type"]} for relatedbill in r_relatedbills]
+
+###############################################################################################################################################################
+##### Bill Subjects
+###############################################################################################################################################################
 
 def get_bill_subjects(congress, bill_type, bill_number):
-    print(f"Getting subjects for {congress}/{bill_type}/{bill_number}")
+    """
+    Fetches and returns subjects for a specific bill.
+    """
+    print(f"Fetching subjects for {congress}/{bill_type}/{bill_number}")
+
+    # Make API request
     r = requests.get(f"https://api.congress.gov/v3/bill/{congress}/{bill_type}/{bill_number}/subjects", params={"api_key": API_KEY})
+
+    # Check for successful request
     if r.status_code != 200:
         print("Error with API request")
+        return {}
+
+    # Extract and return subject details
     r_subjects = r.json()["subjects"]
-    subjects = {"legislative_subjects": [x["name"] for x in r_subjects["legislativeSubjects"]], "policy_area": r_subjects.get("policyArea", {}).get("name")}
-    return subjects
+    return {"legislative_subjects": [x["name"] for x in r_subjects["legislativeSubjects"]], "policy_area": r_subjects.get("policyArea", {}).get("name")}
 
 if __name__ == "__main__":
     get_bills()
